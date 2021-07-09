165970c7f97b876676700ce5cae97acd6cabe430
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-07-08 23:09:08 -0300

    Operadores

---
+++ Operadores/operadores.js
@@ -0,0 +1,12 @@
+//ADIÇÃO
+console.log(2 + 3);
+//SUBTRAÇÃO
+console.log(3 - 2);
+//MULTIPLICAÇÃO
+console.log(2 * 3);
+//DIVISÃO
+console.log(3 / 2);
+//MODULO
+console.log( 3 % 2);
+//EXPONENCIAÇÃO
+console.log(3 ** 2);

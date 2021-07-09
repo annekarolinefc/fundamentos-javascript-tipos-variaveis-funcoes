fb13dc342da34e5145a1990d3682e561ced2d24d
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-07-08 21:02:43 -0300

    Null e undefined

---
+++ Tipos/Tipos primitivos/null.js
@@ -0,0 +1,10 @@
+//NULL pode ser traduzido como "ausencia de valor"
+//pode ser atribuido como valor de uma variável
+
+let input = null;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
---
+++ Tipos/Tipos primitivos/undefined.js
@@ -0,0 +1,10 @@
+let input = null;
+let input2;
+
+console.log(input); // null
+console.log(input2); // undefined
+
+//UNDEFINED também representa “ausência de valor”, porém de uma outra forma: 
+//usualmente, enquanto null é um valor atribuído a uma variável que existe e 
+//foi iniciada, undefined se refere ao valor de uma variável que não foi inicializada 
+//(ou seja, não foi atribuído nenhum valor a ela).

279238c1886315241f7d4d587f771f024e570479
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-07-08 23:55:55 -0300

    operadores

---
+++ Operadores/increment-operator.js
@@ -0,0 +1,20 @@
+//PRE INCREMENTO
+let count = 0
+console.log(++count)        // 1
+console.log(count)          // 1
+
+//POS INCREMENTO
+let count = 0
+console.log(count++)        // 0
+console.log(count)          // 1
+
+
+//PRE DECREMENTO
+let count = 0
+console.log(--count) // -1
+console.log(count)  // -1
+
+//POS DECREMENTO
+let count = 0
+console.log(count--) // 0
+console.log(count)   // -1

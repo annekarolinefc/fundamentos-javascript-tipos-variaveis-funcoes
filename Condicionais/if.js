a6eb03f2603da351facd9ffbe89aaae0576a3bd2
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-07-11 14:12:01 -0300

    Estudo sobre Condicionais

---
+++ Condicionais/if.js
@@ -0,0 +1,11 @@
+//IF: Usado para checar se uma condição é verdadeira e executar o bloco de código.
+
+let num = 3;
+if (num>0){
+    console.log(`${num} is a positive number`)
+}
+
+let isRaining = true;
+if(isRaining){
+    console.log("Remeber to take your rain coat.")
+}

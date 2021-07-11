b5b5e691b216584db51f98a82cd528fae675e2a2
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-07-11 13:24:27 -0300

    Exercicio não finalizado

--- EXERCICIOS/DAY 03/level03/day03-level3.js
+++ EXERCICIOS/DAY 03/level03/day03-level3.js
@@ -1,5 +1,33 @@
-/*Create a human readable time format using the Date time object. The hour and the minute should be all the time two digits(7 hours should be 07 and 5 minutes should be 05 )
-YYY-MM-DD HH:mm eg. 20120-01-02 07:05
+/*
+ERRO
 
+Create a human readable time format using the Date time object. 
+The hour and the minute should be all the time two digits(7 hours should be 07 and 5 minutes should be 05 )
+YYY-MM-DD HH:mm eg. 20120-01-02 07:05
 */
-s
+
+
+var now = new Date();
+
+function getHours(){
+    if(now.getHours()<10){
+        console.log(("0"+now.getHours()))
+    }else{
+        console.log(now.getHours())  
+    }
+}
+
+function getMinutes(){
+    if(now.getMinutes()<10){
+        console.log("0"+now.getMinutes())
+    }else{
+        console.log(now.getMinutes())
+    } 
+}
+var minutes = parseFloat(getMinutes());
+var hour = parseFloat(getHours());
+
+console.log("YYYY-MM-DD HH:mm eg. " + now.getFullYear() + "-" + now.getMonth() + "-" + now.getDay() + " " + hour +":"+ minutes)
+
+
+

b7a192b4988ae2bced33535181362cdecc31e98d
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-07-11 13:01:35 -0300

    Exercício 13 finalizado

---
+++ EXERCICIOS/DAY 03/level02/ex13.js
@@ -0,0 +1,13 @@
+/*
+Create a human readable time format using the Date time object
+
+YYYY-MM-DD HH:mm
+DD-MM-YYYY HH:mm
+DD/MM/YYYY HH:mm
+
+*/
+
+const now = new Date();
+console.log(now.getFullYear() + "-" + now.getMonth() + "-" + now.getDay() + " " + now.getHours() + ":" + now.getMinutes());
+console.log(now.getDay() + "-" + now.getMonth() + "-" +  now.getFullYear()+ " " + now.getHours() + ":" + now.getMinutes());
+console.log(now.getDay() + "/" + now.getMonth() + "/" +  now.getFullYear()+ " " + now.getHours() + ":" + now.getMinutes());
